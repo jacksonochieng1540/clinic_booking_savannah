1025b396ca67a8bf3fb21ae6b6c84018a3cdd7f7
Jackson ochieng <notoriousmma@Notoriousmma.local>
2026-08-04 23:34:58 +0300

    fix: remove the duplicated sqlite database

--- clinic_booking/settings.py
+++ clinic_booking/settings.py
@@ -109,13 +109,6 @@ WSGI_APPLICATION = "clinic_booking.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 
 if os.getenv("DATABASE_URL"):
     DATABASES = {"default": dj_database_url.config(default=os.getenv("DATABASE_URL"), conn_max_age=600)}
